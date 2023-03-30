0022ec990d0e2fdfc46b53ac7f81801dac7096d2
Alex Amiryan <git@fenritz.com>
2023-03-30 05:16:29 +0400

    Fix in dockerUpdate script

--- bin/dockerUpdate.sh
+++ bin/dockerUpdate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source .env
-docker pull
+docker pull stingle/stingle-api:latest
 docker compose $COMPOSE_PARAMS -p $CONTAINER_NAME stop web && docker compose -p $CONTAINER_NAME up -d web
