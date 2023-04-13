acc356c62c70a82d751262ad5381fcacb0deab5b
Alex Amiryan <git@fenritz.com>
2023-04-13 04:23:27 +0400

    Fix in update

--- bin/update.sh
+++ bin/update.sh
@@ -2,6 +2,6 @@
 source .env
 git pull
 docker pull stingle/stingle-api:latest
-docker compose $COMPOSE_PARAMS -p $CONTAINER_NAME stop web && docker compose $COMPOSE_PARAMS -p $CONTAINER_NAME start web
+docker compose $COMPOSE_PARAMS -p $CONTAINER_NAME up -d
 for dir in addons/*; do (cd "$dir" && git pull); done
 bin/deleteDockerCache.sh
